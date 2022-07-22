fe0ed1657a33588b084c58d327ab32c3d7cfeaca
Kaushalendra Pratap Singh <kaushalendrapratapsingh92@gmail.com>
2022-07-22 18:53:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App(){
   return(
     <div className='app'>
       <Header />
+      //Edit OnClick setting on download button it's not working!
       <Content />
     </div>
   )
